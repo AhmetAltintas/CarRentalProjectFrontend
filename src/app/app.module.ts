69924a6ebd402d22959d9d4bfcd3e6eb537e8ae4
Ahmet ALTINTAŞ <ahmtaltnts35@gmail.com>
2022-11-23 10:41:13 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CarImageComponent } from './components/car-image/car-image.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { RentComponent } from './components/rent/rent.component';
+import { UserComponent } from './components/user/user.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    ColorComponent,
+    CustomerComponent,
+    CarImageComponent,
+    BrandComponent,
+    RentComponent,
+    UserComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
