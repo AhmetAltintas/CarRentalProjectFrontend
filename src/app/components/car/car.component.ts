8e65f341cf111ed63ef50329c9ef71d10590120b
Ahmet ALTINTAŞ <ahmtaltnts35@gmail.com>
2022-11-25 08:26:31 +0300

    .

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -11,15 +11,41 @@ import { CarService } from 'src/app/services/car.service';
 export class CarComponent implements OnInit {
   cars: Car[] = [];
   dataLoaded = false;
-  constructor(private carService: CarService,
-    private activatedRoute:ActivatedRoute) {}
+  constructor(
+    private carService: CarService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.getCars();
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['brandId']) {
+        this.getCarsByBrand(params['brandId']);
+      } 
+      else if (params['colorId']) {
+        this.getCarsByColor(params['colorId']);
+      } 
+      else {
+        this.getCars();
+      }
+    });
   }
 
   getCars() {
-    this.carService.getCars().subscribe(response => {
+    this.carService.getCars().subscribe((response) => {
+      this.cars = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getCarsByColor(colorId: number) {
+    this.carService.getCarsByColor(colorId).subscribe((response) => {
+      this.cars = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getCarsByBrand(brandId: number) {
+    this.carService.getCarsByBrand(brandId).subscribe((response) => {
       this.cars = response.data;
       this.dataLoaded = true;
     });
